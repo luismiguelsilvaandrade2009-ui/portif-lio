b6dd896faa3e38d1691978eeeaf9b7fb2d13b573
Luis miguel <luismiguelsilvaandrade2009@gmail.com>
2026-02-27 23:37:18 -0300

    botoes navbar
    coloquei funçoes em cada botao da nav bar

--- javaS.js
+++ javaS.js
@@ -1,13 +1,56 @@
 const header = document.querySelector('.navbar')
 
-window.onscroll = function(){
+window.onscroll = function () {
     let top = window.scrollY;
-    if(header){
-        if(top>=140){
+    if (header) {
+        if (top >= 140) {
             header.classList.add('navbarDark')
         }
-        else{
+        else {
             header.classList.remove('navbarDark')
         }
     }
 }
+
+// quando o usuário clicar no link "Home", não navega para o topo
+// em vez disso rola suavemente até a seção "Sobre mim" (id="Sobre-mim").
+const homeLink = document.querySelector('a.nav-link[href="#about"]');
+if (homeLink) {
+    homeLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const aboutSection = document.getElementById('Sobre-mim');
+        if (aboutSection) {
+            aboutSection.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+}
+
+
+const projectLink = document.querySelector('a.nav-link[href="#services"]');
+if (projectLink) {
+    projectLink.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const projectSection = document.getElementById('proj'); // id da seção de projetos
+        if (projectSection) {
+            projectSection.scrollIntoView({ behavior: 'smooth' }); // rola até ela
+        }
+    });
+}
+
+const contactLink = document.querySelector('a.nav-link[href="#contact"]')
+if (contactLink)
+         { contactLink.addEventListener('click', function (q) {
+            q.preventDefault();
+
+            const contactSection = document.getElementById('ctt')
+            if (contactSection) {
+                contactSection.scrollIntoView({ behavior: 'smooth' })
+            }
+
+
+
+        })}
+
+       
+    
